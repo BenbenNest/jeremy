5bc75ba4d9fdf9afeca6f461f43f8502564747e7
changqing.zhao <changqing.zhao@nio.com>
2017-03-04 18:05:41 +0800

    shake fun fix

--- demo/src/main/java/com/jeremy/demo/activity/ShakeActivity.java
+++ demo/src/main/java/com/jeremy/demo/activity/ShakeActivity.java
@@ -24,17 +24,12 @@ import com.jeremy.demo.R;
 
 import java.lang.ref.WeakReference;
 
-/**
- * 参考：http://www.cnblogs.com/lr393993507/p/5543049.html android 实现摇一摇功能
- */
 @SuppressLint("HandlerLeak")
 public class ShakeActivity extends AppCompatActivity implements SensorEventListener {
     private static final String TAG = "ShakeActivity";
 
-    private SensorManager sensorManager;
-    private Sensor sensor;
     private static final int UPTATE_INTERVAL_TIME = 50;
-    private static final int SPEED_SHRESHOLD = 50;//这个值调节灵敏度
+    private static final int SPEED_SHRESHOLD = 50;//调节灵敏度
     private long lastUpdateTime;
     private float lastX;
     private float lastY;
@@ -46,58 +41,33 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
 
     private SensorManager mSensorManager;
     private Sensor mAccelerometerSensor;
-    private Vibrator mVibrator;//手机震动
-    private SoundPool mSoundPool;//摇一摇音效
-    //记录摇动状态
+    private Vibrator mVibrator;
+    private SoundPool mSoundPool;
     private boolean isShake = false;
     private LinearLayout mTopLayout;
     private LinearLayout mBottomLayout;
     private ImageView mTopLine;
     private ImageView mBottomLine;
-
     private MyHandler mHandler;
     private int mWeiChatAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //设置只竖屏
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_shake);
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        //初始化View
         initView();
         mHandler = new MyHandler(this);
-
-        //初始化SoundPool
         mSoundPool = new SoundPool(1, AudioManager.STREAM_SYSTEM, 5);
         mWeiChatAudio = mSoundPool.load(this, R.raw.weichat_audio, 1);
-
-        //获取Vibrator震动服务
         mVibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (sensorManager != null) {
-            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            if (sensor != null) {
-                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_GAME);//这里选择感应频率
-            }
-        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //获取 SensorManager 负责管理传感器
         mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
         if (mSensorManager != null) {
-            //获取加速度传感器
             mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             if (mAccelerometerSensor != null) {
                 mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_UI);
@@ -107,8 +77,6 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
 
     @Override
     protected void onPause() {
-        // 务必要在pause中注销 mSensorManager
-        // 否则会造成界面退出后摇一摇依旧生效的bug
         if (mSensorManager != null) {
             mSensorManager.unregisterListener(this);
         }
@@ -116,26 +84,17 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
     }
 
     private void initView() {
-
         mTopLayout = (LinearLayout) findViewById(R.id.main_linear_top);
         mBottomLayout = ((LinearLayout) findViewById(R.id.main_linear_bottom));
         mTopLine = (ImageView) findViewById(R.id.main_shake_top_line);
         mBottomLine = (ImageView) findViewById(R.id.main_shake_bottom_line);
-
-        //默认
         mTopLine.setVisibility(View.GONE);
         mBottomLine.setVisibility(View.GONE);
-
-
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-    // SensorEventListener回调方法
-    ///////////////////////////////////////////////////////////////////////////
     @Override
     public void onSensorChanged(SensorEvent event) {
         int type = event.sensor.getType();
-
         if (type == Sensor.TYPE_ACCELEROMETER) {
             long currentUpdateTime = System.currentTimeMillis();
             long timeInterval = currentUpdateTime - lastUpdateTime;
@@ -143,7 +102,6 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
                 return;
             }
             lastUpdateTime = currentUpdateTime;
-            //获取三个方向值
             float[] values = event.values;
             float x = values[0];
             float y = values[1];
@@ -157,27 +115,20 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
             lastZ = z;
             double speed = (Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ) / timeInterval) * 100;
 
-//            if ((Math.abs(x) > 17 || Math.abs(y) > 17 || Math
-//                    .abs(z) > 17) && !isShake) {
+//            if ((Math.abs(x) > 17 || Math.abs(y) > 17 || Math.abs(z) > 17) && !isShake) {
             if (speed >= SPEED_SHRESHOLD && !isShake) {
                 isShake = true;
-                // TODO: 2016/10/19 实现摇动逻辑, 摇动后进行震动
                 Thread thread = new Thread() {
                     @Override
                     public void run() {
                         super.run();
                         try {
-                            Log.d(TAG, "onSensorChanged: 摇动");
-
-                            //开始震动 发出提示音 展示动画效果
+                            Log.d(TAG, "onSensorChanged");
                             mHandler.obtainMessage(START_SHAKE).sendToTarget();
                             Thread.sleep(500);
-                            //再来一次震动提示
                             mHandler.obtainMessage(AGAIN_SHAKE).sendToTarget();
                             Thread.sleep(500);
                             mHandler.obtainMessage(END_SHAKE).sendToTarget();
-
-
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -191,7 +142,6 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
-
 
     private static class MyHandler extends Handler {
         private WeakReference<ShakeActivity> mReference;
@@ -209,36 +159,25 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
             super.handleMessage(msg);
             switch (msg.what) {
                 case START_SHAKE:
-                    //This method requires the caller to hold the permission VIBRATE.
-//                    mActivity.mVibrator.vibrate(300);
-                    //发出提示音
+                    mActivity.mVibrator.vibrate(300);
                     mActivity.mSoundPool.play(mActivity.mWeiChatAudio, 1, 1, 0, 0, 1);
                     mActivity.mTopLine.setVisibility(View.VISIBLE);
                     mActivity.mBottomLine.setVisibility(View.VISIBLE);
-                    mActivity.startAnimation(false);//参数含义: (不是回来) 也就是说两张图片分散开的动画
+                    mActivity.startAnimation(false);
                     break;
                 case AGAIN_SHAKE:
-//                    mActivity.mVibrator.vibrate(300);
+                    mActivity.mVibrator.vibrate(300);
                     break;
                 case END_SHAKE:
-                    //整体效果结束, 将震动设置为false
                     mActivity.isShake = false;
-                    // 展示上下两种图片回来的效果
                     mActivity.startAnimation(true);
                     break;
             }
         }
     }
 
-    /**
-     * 开启 摇一摇动画
-     *
-     * @param isBack 是否是返回初识状态
-     */
     private void startAnimation(boolean isBack) {
-        //动画坐标移动的位置的类型是相对自己的
         int type = Animation.RELATIVE_TO_SELF;
-
         float topFromY;
         float topToY;
         float bottomFromY;
@@ -255,22 +194,18 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
             bottomToY = 0.5f;
         }
 
-        //上面图片的动画效果
         TranslateAnimation topAnim = new TranslateAnimation(
                 type, 0, type, 0, type, topFromY, type, topToY
         );
         topAnim.setDuration(200);
-        //动画终止时停留在最后一帧~不然会回到没有执行之前的状态
         topAnim.setFillAfter(true);
 
-        //底部的动画效果
         TranslateAnimation bottomAnim = new TranslateAnimation(
                 type, 0, type, 0, type, bottomFromY, type, bottomToY
         );
         bottomAnim.setDuration(200);
         bottomAnim.setFillAfter(true);
 
-        //大家一定不要忘记, 当要回来时, 我们中间的两根线需要GONE掉
         if (isBack) {
             bottomAnim.setAnimationListener(new Animation.AnimationListener() {
                 @Override
@@ -283,57 +218,13 @@ public class ShakeActivity extends AppCompatActivity implements SensorEventListe
 
                 @Override
                 public void onAnimationEnd(Animation animation) {
-                    //当动画结束后 , 将中间两条线GONE掉, 不让其占位
                     mTopLine.setVisibility(View.GONE);
                     mBottomLine.setVisibility(View.GONE);
                 }
             });
         }
-        //设置动画
         mTopLayout.startAnimation(topAnim);
         mBottomLayout.startAnimation(bottomAnim);
-
     }
-
-    private SensorEventListener sensorEventListener = new SensorEventListener() {
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-
-
-            //这是优化后的算法
-//            long currentUpdateTime = System.currentTimeMillis();
-//            long timeInterval = currentUpdateTime - lastUpdateTime;
-//            if (timeInterval < UPTATE_INTERVAL_TIME) {
-//                return;
-//            }
-//            lastUpdateTime = currentUpdateTime;
-//            // 传感器信息改变时执行该方法
-//            float[] values = event.values;
-//            float x = values[0]; // x轴方向的重力加速度，向右为正
-//            float y = values[1]; // y轴方向的重力加速度，向前为正
-//            float z = values[2]; // z轴方向的重力加速度，向上为正
-//            float deltaX = x - lastX;
-//            float deltaY = y - lastY;
-//            float deltaZ = z - lastZ;
-//            lastX = x;
-//            lastY = y;
-//            lastZ = z;
-//            double speed = (Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ) / timeInterval) * 100;
-//            if (speed >= SPEED_SHRESHOLD) {
-////                vibrator.vibrate(300);
-////                image.setImageResource(R.drawable.running01);
-//            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-
-        }
-
-
-    };
-
 
 }
