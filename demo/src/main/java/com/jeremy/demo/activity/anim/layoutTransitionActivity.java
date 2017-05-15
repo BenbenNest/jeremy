1c550c03291d6d8b4f9967a76a87c78b5b2ee9eb
changqing.zhao <changqing.zhao@nio.com>
2017-05-15 14:01:24 +0800

    temp

---
+++ demo/src/main/java/com/jeremy/demo/activity/anim/layoutTransitionActivity.java
@@ -0,0 +1,15 @@
+package com.jeremy.demo.activity.anim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jeremy.demo.R;
+
+public class layoutTransitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_transition);
+    }
+}
