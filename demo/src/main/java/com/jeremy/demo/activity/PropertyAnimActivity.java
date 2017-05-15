e71205bfc36e752d3f3bbe3d03817b8772d08870
changqing.zhao <changqing.zhao@nio.com>
2017-05-15 14:02:18 +0800

    temp2

--- demo/src/main/java/com/jeremy/demo/activity/PropertyAnimActivity.java
+++ demo/src/main/java/com/jeremy/demo/activity/PropertyAnimActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 
 import com.jeremy.demo.R;
 import com.jeremy.demo.activity.anim.Xml4AnimActivity;
+import com.jeremy.demo.activity.anim.layoutTransitionActivity;
 
 /**
  * anim文件夹下存放tween animation和frame animation；
@@ -13,12 +14,12 @@ import com.jeremy.demo.activity.anim.Xml4AnimActivity;
  * 在代码中使用AnimationUtils.loadAnimation（）方法加载；
  * 使用mView.setAnimation(mAnimation)为mView加载动画；
  * 使用mView.startAnimation()开启动画；
-
- animator文件夹下存放property animation，即属性动画，
- xml文件里有animator、objectAnimator和set三个标签；
- 在代码中使用AnimatorInflater.loadAnimator（）方法加载动画；
- 使用mAnimation.setTarget（mView）为mView加载动画。
- 使用mAnimation.start（）开启动画
+ * <p>
+ * animator文件夹下存放property animation，即属性动画，
+ * xml文件里有animator、objectAnimator和set三个标签；
+ * 在代码中使用AnimatorInflater.loadAnimator（）方法加载动画；
+ * 使用mAnimation.setTarget（mView）为mView加载动画。
+ * 使用mAnimation.start（）开启动画
  */
 public class PropertyAnimActivity extends AppCompatActivity {
 
@@ -32,6 +33,10 @@ public class PropertyAnimActivity extends AppCompatActivity {
 
     public void XmlAnimation(View view) {
         Xml4AnimActivity.startActivity(this);
+    }
+
+    public void layoutTransition(View view) {
+        layoutTransitionActivity.startActivity(this);
     }
 
 }
--- demo/src/main/java/com/jeremy/demo/activity/anim/layoutTransitionActivity.java
+++ demo/src/main/java/com/jeremy/demo/activity/anim/layoutTransitionActivity.java
@@ -1,15 +1,164 @@
 package com.jeremy.demo.activity.anim;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.Keyframe;
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.jeremy.demo.R;
 
 public class layoutTransitionActivity extends AppCompatActivity {
+    private int i = 0;
+
+    private LinearLayout container;
+
+    private LayoutTransition mTransitioner;
+
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, layoutTransitionActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_transition);
+        container = (LinearLayout) findViewById(R.id.parent);
+
+        initTransition();
+        setTransition();
+    }
+
+
+    /**
+     * 初始化容器动画
+     */
+    private void initTransition() {
+        mTransitioner = new LayoutTransition();
+        container.setLayoutTransition(mTransitioner);
+    }
+
+
+    private void setTransition() {
+        /**
+         * view出现时 view自身的动画效果
+         */
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(null, "rotationY", 90F, 0F).
+                setDuration(mTransitioner.getDuration(LayoutTransition.APPEARING));
+        mTransitioner.setAnimator(LayoutTransition.APPEARING, animator1);
+
+        /**
+         * view 消失时，view自身的动画效果
+         */
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(null, "rotationX", 0F, 90F, 0F).
+                setDuration(mTransitioner.getDuration(LayoutTransition.DISAPPEARING));
+        mTransitioner.setAnimator(LayoutTransition.DISAPPEARING, animator2);
+
+        /**
+         * view 动画改变时，布局中的每个子view动画的时间间隔
+         */
+        mTransitioner.setStagger(LayoutTransition.CHANGE_APPEARING, 30);
+        mTransitioner.setStagger(LayoutTransition.CHANGE_DISAPPEARING, 30);
+
+
+        /**
+         * 为什么这里要这么写？具体我也不清楚，ViewGroup源码里面是这么写的，我只是模仿而已
+         * 不这么写貌似就没有动画效果了，所以你懂的！
+         */
+        PropertyValuesHolder pvhLeft = PropertyValuesHolder.ofInt("left", 0, 1);
+        PropertyValuesHolder pvhTop =
+                PropertyValuesHolder.ofInt("top", 0, 1);
+        PropertyValuesHolder pvhRight =
+                PropertyValuesHolder.ofInt("right", 0, 1);
+        PropertyValuesHolder pvhBottom =
+                PropertyValuesHolder.ofInt("bottom", 0, 1);
+
+
+        /**
+         * view出现时，导致整个布局改变的动画
+         */
+        PropertyValuesHolder animator3 = PropertyValuesHolder.ofFloat("scaleX", 1F, 2F, 1F);
+        final ObjectAnimator changeIn = ObjectAnimator.ofPropertyValuesHolder(
+                this, pvhLeft, pvhTop, pvhRight, pvhBottom, animator3).
+                setDuration(mTransitioner.getDuration(LayoutTransition.CHANGE_APPEARING));
+        changeIn.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                View view = (View) ((ObjectAnimator) animation).getTarget();
+                view.setScaleX(1.0f);
+            }
+        });
+        mTransitioner.setAnimator(LayoutTransition.CHANGE_APPEARING, changeIn);
+
+
+        /**
+         * view消失，导致整个布局改变时的动画
+         */
+        Keyframe kf0 = Keyframe.ofFloat(0f, 0f);
+        Keyframe kf1 = Keyframe.ofFloat(.5f, 2f);
+        Keyframe kf2 = Keyframe.ofFloat(1f, 0f);
+        PropertyValuesHolder pvhRotation =
+                PropertyValuesHolder.ofKeyframe("scaleX", kf0, kf1, kf2);
+        final ObjectAnimator changeOut = ObjectAnimator.ofPropertyValuesHolder(
+                this, pvhLeft, pvhTop, pvhRight, pvhBottom, pvhRotation).
+                setDuration(mTransitioner.getDuration(LayoutTransition.CHANGE_DISAPPEARING));
+        changeOut.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                View view = (View) ((ObjectAnimator) animation).getTarget();
+                view.setScaleX(1.0f);
+            }
+        });
+        mTransitioner.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, changeOut);
+    }
+
+
+    public void buttonClick(View view) {
+        addButtonView();
+    }
+
+    public void buttonClick1(View view) {
+        removeButtonView();
+    }
+
+    private void addButtonView() {
+        i++;
+        Button button = new Button(this);
+        button.setText("button" + i);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        container.addView(button, Math.min(1, container.getChildCount()), params);
+    }
+
+    private void removeButtonView() {
+        if (i > 0)
+            container.removeViewAt(0);
+    }
+
+
+    private void initAinm() {
+        //通过加载XML动画设置文件来创建一个Animation对象；
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.left);
+        //得到一个LayoutAnimationController对象；
+        LayoutAnimationController lac = new LayoutAnimationController(animation);
+        //设置控件显示的顺序；
+        lac.setOrder(LayoutAnimationController.ORDER_REVERSE);
+        //设置控件显示间隔时间；
+        lac.setDelay(1);
+        //为ListView设置LayoutAnimationController属性；
+//        listView.setLayoutAnimation(lac);
     }
 }
