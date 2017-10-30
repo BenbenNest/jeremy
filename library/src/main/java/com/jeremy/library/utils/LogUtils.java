a6283adb132fdac4ee5cac42d3110850b718e6a8
jeremy <zhaochangqing@didichuxing.com>
2017-10-30 10:07:42 +0800

    超过长度的log记录

---
+++ library/src/main/java/com/jeremy/library/utils/LogUtils.java
@@ -0,0 +1,34 @@
+package com.jeremy.library.utils;
+
+import android.util.Log;
+
+/**
+ * Created by changqing on 2017/10/27.
+ */
+
+public class LogUtils {
+
+
+
+    // 使用Log来显示调试信息,因为log在实现上每个message有4k字符长度限制
+    // 所以这里使用自己分节的方式来输出足够长度的message
+    public static void log_split(String str) {
+        str = str.trim();
+        int index = 0;
+        int maxLength = 4000;
+        String sub;
+        while (index < str.length()) {
+            // java的字符不允许指定超过总的长度end
+            if (str.length() <= index + maxLength) {
+                sub = str.substring(index);
+            } else {
+                sub = str.substring(index, maxLength);
+            }
+            index += maxLength;
+            Log.i("jeremy_split", sub.trim());
+        }
+    }
+
+
+
+}
