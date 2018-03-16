35673564a2fea3bb68fd83bb2e94677037d1c34a
jeremy <zhaochangqing@didichuxing.com>
2018-03-16 19:23:18 +0800

    simple factory

---
+++ demo/src/main/java/com/jeremy/demo/designpattern/simplefactory/Product.java
@@ -0,0 +1,9 @@
+package com.jeremy.demo.designpattern.simplefactory;
+
+/**
+ * Created by changqing on 2018/3/15.
+ */
+
+public interface Product {
+
+}
---
+++ demo/src/main/java/com/jeremy/demo/designpattern/simplefactory/SimpleFactory.java
@@ -0,0 +1,12 @@
+package com.jeremy.demo.designpattern.simplefactory;
+
+/**
+ * Created by changqing on 2018/3/15.
+ */
+
+public class SimpleFactory {
+
+
+
+
+}
