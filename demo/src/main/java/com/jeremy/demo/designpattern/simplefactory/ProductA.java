9f7e6dad611efb3c9ef7bcab4b3821b63402837a
jeremy <zhaochangqing@didichuxing.com>
2018-03-21 20:33:25 +0800

    fix simpleFactory

---
+++ demo/src/main/java/com/jeremy/demo/designpattern/simplefactory/ProductA.java
@@ -0,0 +1,12 @@
+package com.jeremy.demo.designpattern.simplefactory;
+
+/**
+ * Created by changqing on 2018/3/21.
+ */
+
+public class ProductA implements Product {
+    @Override
+    public void print() {
+        System.out.print("ProductA");
+    }
+}
--- demo/src/main/java/com/jeremy/demo/designpattern/simplefactory/SimpleFactory.java
+++ demo/src/main/java/com/jeremy/demo/designpattern/simplefactory/SimpleFactory.java
@@ -6,7 +6,18 @@ package com.jeremy.demo.designpattern.simplefactory;
 
 public class SimpleFactory {
 
-
-
+    public Product create(String type) {
+        Product product = null;
+        if (type == null) {
+            throw new RuntimeException("请输入产品类型");
+        } else if (type.equals("A")) {
+            product = new ProductA();
+        } else if (type.equals("B")) {
+            product = new ProductB();
+        } else {
+            throw new RuntimeException("暂不支持该产品类型");
+        }
+        return product;
+    }
 
 }
