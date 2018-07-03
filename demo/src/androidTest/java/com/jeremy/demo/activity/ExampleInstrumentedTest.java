1126efaa288eb7976ce2ccb0e3d0f38b548bba2f
jeremy <zhaochangqing@didichuxing.com>
2018-07-03 16:15:33 +0800

    fix android test gradle

---
+++ demo/src/androidTest/java/com/jeremy/demo/activity/ExampleInstrumentedTest.java
@@ -0,0 +1,25 @@
+package com.jeremy.demo.activity;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by changqing on 2018/7/3.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.yangge.myapplication", appContext.getPackageName());
+    }
+}
