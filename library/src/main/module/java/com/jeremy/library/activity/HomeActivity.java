61ee498a969fa39f7b4e5f6f646ea48b9c81a0b2
jeremy <zhaochangqing@didichuxing.com>
2018-08-01 13:48:02 +0800

    配置sourceSet

---
+++ library/src/main/module/java/com/jeremy/library/activity/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.jeremy.library.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by changqing on 2018/8/1.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+}
---
+++ library/src/main/module/java/com/jeremy/library/activity/ModuleApplication.java
@@ -0,0 +1,12 @@
+package com.jeremy.library.activity;
+
+import android.app.Application;
+
+/**
+ * Created by changqing on 2018/8/1.
+ */
+
+public class ModuleApplication extends Application {
+
+
+}
